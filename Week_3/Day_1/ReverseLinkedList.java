cc4905048269907fc234ab6500a56ed5a989b69b
rajveer0632 <rvsingh0632@gmail.com>
2026-06-15 13:41:22 +0530

    Week 3,Day 1 done.

---
+++ Week_3/Day_1/ReverseLinkedList.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode current=head;
+        ListNode prev=null;
+        while(current!=null)
+        {
+            ListNode temp=current.next;
+            current.next=prev;
+            prev=current;
+            current=temp;
+        }
+        return prev;
+    }
+}
