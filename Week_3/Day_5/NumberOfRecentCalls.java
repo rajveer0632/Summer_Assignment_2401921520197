9e59cdc7ff32c9a292504daa65ca44f6c16184f5
rajveer0632 <rvsingh0632@gmail.com>
2026-06-19 15:45:32 +0530

    Week 3, Day 5 Done.

---
+++ Week_3/Day_5/NumberOfRecentCalls.java
@@ -0,0 +1,18 @@
+class RecentCounter 
+{
+    private Queue<Integer> q;
+    public RecentCounter() 
+    {
+        q = new LinkedList<>();
+    }
+    
+    public int ping(int t) 
+    {
+        q.add(t);
+        while (!q.isEmpty() && q.peek() < t - 3000)
+        {
+            q.poll(); 
+        }
+        
+        return q.size();
+    }
